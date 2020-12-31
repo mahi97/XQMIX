cf001bf0b9c05fc0292b9a80ba35e548a5724af0
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-12-31 20:45:22 +0900

    add agents to __init__

--- src/modules/agents/__init__.py
+++ src/modules/agents/__init__.py
@@ -1,4 +1,9 @@
 REGISTRY = {}
 
 from .rnn_agent import RNNAgent
+from .noisy_agent import NoisyRNNAgent
+from .ntm_agent import NTMAgent
+
 REGISTRY["rnn"] = RNNAgent
+REGISTRY["ntm"] = NTMAgent
+REGISTRY["noisy-rnn"] = NoisyRNNAgent
