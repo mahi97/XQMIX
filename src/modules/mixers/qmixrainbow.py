b7c37d911a9515ad12a4c0adec2948a57f04dc2d
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-12-31 20:45:32 +0900

    add new quadratic mixer

---
+++ src/modules/mixers/qmixrainbow.py
@@ -0,0 +1,58 @@
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class QMixerRainbow(nn.Module):
+    def __init__(self, args):
+        super(QMixerRainbow, self).__init__()
+
+        self.args = args
+        self.n_agents = args.n_agents
+        self.state_dim = int(np.prod(args.state_shape))
+
+        self.embed_dim = args.mixing_embed_dim
+
+        self.hyper_w_1 = nn.Linear(self.state_dim, self.embed_dim * self.n_agents)
+        self.hyper_w_final = nn.Linear(self.state_dim, self.embed_dim)
+
+        # State dependent bias for hidden layer
+        self.hyper_b_1 = nn.Linear(self.state_dim, self.embed_dim)
+
+        # V(s) instead of a bias for the last layers
+        self.V = nn.Sequential(nn.Linear(self.state_dim, self.embed_dim),
+                               nn.ReLU(),
+                               nn.Linear(self.embed_dim, 1))
+
+    def forward(self, agent_qs, states):
+        bs = agent_qs.size(0)
+        states = states.reshape(-1, self.state_dim)
+        agent_qs = agent_qs.view(-1, 1, self.n_agents)
+        # First layer
+        w1 = self.pos_func(self.hyper_w_1(states))
+        #print(w1[0])
+        b1 = self.hyper_b_1(states)
+        w1 = w1.view(-1, self.n_agents, self.embed_dim)
+        b1 = b1.view(-1, 1, self.embed_dim)
+        hidden = F.elu(th.bmm(agent_qs, w1) + b1)
+        # Second layer
+        w_final = self.pos_func(self.hyper_w_final(states))
+        w_final = w_final.view(-1, self.embed_dim, 1)
+        # State-dependent bias
+        v = self.V(states).view(-1, 1, 1)
+        # Compute final output
+        y = th.bmm(hidden, w_final) + v
+        # Reshape and return
+        q_tot = y.view(bs, -1, 1)
+        return q_tot
+
+    def pos_func(self, x):
+        if self.args.qmix_plus_func == "softplus":
+            return th.nn.Softplus(beta=self.args.qmix_plus_beta)(x)
+        elif self.args.qmix_plus_func == "quadratic":
+            return 0.5 * x ** 2
+        elif self.args.qmix_plus_func == "abs":
+            return th.abs(x)
+        else:
+            raise ValueError("Unknown qmix plus func {}".format(self.args.qmix_plus_func))
